83615e5779eb2b476d4ea37e1f06a4437c9b9af9
Gin-MC <cotosgino@gmail.com>
2025-05-30 15:29:25 -0500

    links de imagenes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv';//carga las variables de entorno desde un archivo .env
 
-dotenv.config();
+dotenv.config();//Ejecuta la configuración de dotenv
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,// = URL de conexión a la base de datos
 });
 
 export default pool;
